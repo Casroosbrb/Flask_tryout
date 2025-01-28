711e9cb52e5d8d425ec988c8b36d80232b229ef3
Cas <147439216+Casroosbrb@users.noreply.github.com>
2025-01-28 08:24:29 +0100

    s

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Welkom op mijn Flask-website!</p>"
+    return '''
+    <html>
+        <body>
+            <p>Welkom op mijn Flask-website! Hieronder heb je de foto van de opdracht</p>
+            <img src="/image" alt="Afbeelding">
+        </body>
+    </html>
+    '''
+
+@app.route('/image')
+def serve_image():
+    return send_from_directory('.', 'image.png')
 
 if __name__ == "__main__":
     app.run(debug=True)
